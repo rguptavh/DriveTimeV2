ba30e6e42acaeebb84822224e2c6e5ec8589c4cc
rguptavh <rguptavh119@gmail.com>
2020-04-14 00:57:09 -0500

    font

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -4,7 +4,6 @@ import moment from 'moment';
 import * as Progress from 'react-native-progress';
 import DatePicker from 'react-native-datepicker'
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
-import ScalableText from 'react-native-text';
 
 
 
@@ -20,6 +19,12 @@ export default class Login extends React.Component {
     date: '',
     hoursneeded: '-',
   };
+  constructor() {
+    super();
+    Text.defaultProps = Text.defaultProps || {};
+    // Ignore dynamic type scaling on iOS
+    Text.defaultProps.allowFontScaling = false; 
+ }
   componentDidMount() {
     setTimeout(() => {
       this.setState({ progress1: (hours * 60 + minutes) / 2400 });
@@ -29,11 +34,15 @@ export default class Login extends React.Component {
   }
   static navigationOptions = { headerMode: 'none', gestureEnabled: false };
   render() {
+    
     const entireScreenHeight = Dimensions.get('window').height;
     const rem = entireScreenHeight / 380;
     const entireScreenWidth = Dimensions.get('window').width;
     const wid = entireScreenWidth / 380;
     var ree;
+    const onPress = () => {
+      this.props.navigation.navigate('Main')
+    }
     if (entireScreenWidth >= 0.92 * entireScreenHeight * 4 / 9 * 1524 / 1200) {
       ree = rem;
     }
@@ -46,138 +55,163 @@ export default class Login extends React.Component {
       <View style={styles.container}>
 
         <ImageBackground source={require('../assets/login.png')} style={styles.image}>
-          <View style={{ flex: 1, width: '100%', marginTop: '12%', }}>
-            <View style={{ flex: 16, width: '100%', alignItems: 'center' }}>
-              <View style={styles.topcard}>
-                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', flexDirection: 'row', maxWidth: '90%' }}>
-                  <ScalableText style={{ marginTop: '10%' }}>
-                    <ScalableText style={{ fontSize: 55, fontFamily: 'WSR', color: 'white' }}>{hours}</ScalableText>
-                    <ScalableText style={{ fontSize: 30, fontFamily: 'WSR', color: 'white' }}>hours</ScalableText>
-                    <ScalableText style={{ fontSize: 55, fontFamily: 'WSR', color: 'white' }}> {minutes}</ScalableText>
-                    <ScalableText style={{ fontSize: 30, fontFamily: 'WSR', color: 'white' }}>minutes</ScalableText>
-                  </ScalableText>
+          <View style={{ flex: 16, width: '100%', alignItems: 'center', marginTop: entireScreenHeight*0.05, justifyContent: 'center' }}>
+            <View style={styles.topcard}>
+              <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', flexDirection: 'row', maxWidth: '90%' }}>
+                <Text style={{ marginTop: '10%' }}>
+                  <Text style={{ fontSize: 55*wid, fontFamily: 'WSR', color: 'white' }}>{hours}</Text>
+                  <Text style={{ fontSize: 30, fontFamily: 'WSR', color: 'white' }}>hours</Text>
+                  <Text style={{ fontSize: 55, fontFamily: 'WSR', color: 'white' }}>{minutes}</Text>
+                  <Text style={{ fontSize: 30, fontFamily: 'WSR', color: 'white' }}>minutes</Text>
+                </Text>
+              </View>
+              <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', flexDirection: 'row' }}>
+                <Progress.Bar progress={this.state.progress1} width={entireScreenWidth * 0.8} animated={true} height={rem * 20} borderRadius={25} color='#BBE2FF' borderColor='#D0D0D0' unfilledColor='white' />
+              </View>
+            </View>
+          </View>
+          <View style={{ flex: 12, width: '100%', alignItems: 'center' }}>
+            <View style={styles.midcard}>
+              <View style={{ height: '100%', flex: 2 }}>
+                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', flexDirection: 'row' }}>
+                  <Text style={{ marginTop: '5%' }}>
+                    <Text style={{ fontSize: wid * 40, fontFamily: 'WSR', color: 'white' }}>{hours}</Text>
+                    <Text style={{ fontSize: wid * 20, fontFamily: 'WSR', color: 'white' }}>hours</Text>
+                    <Text style={{ fontSize: wid * 40, fontFamily: 'WSR', color: 'white' }}>{minutes}</Text>
+                    <Text style={{ fontSize: wid * 20, fontFamily: 'WSR', color: 'white' }}>minutes</Text>
+                  </Text>
                 </View>
                 <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', flexDirection: 'row' }}>
-                  <Progress.Bar progress={this.state.progress1} width={entireScreenWidth * 0.8} animated={true} height={rem * 20} borderRadius={25} color='#BBE2FF' borderColor='#D0D0D0' unfilledColor='white' />
+                  <Progress.Bar progress={this.state.progress2} width={entireScreenWidth * 0.5} animated={true} height={rem * 15} borderRadius={25} color='#BBE2FF' borderColor='#D0D0D0' unfilledColor='white' />
                 </View>
               </View>
-            </View>
-            <View style={{ flex: 12, width: '100%', alignItems: 'center' }}>
-              <View style={styles.midcard}>
-                <View style={{ height: '100%', flex: 2 }}>
-                  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', flexDirection: 'row' }}>
-                    <ScalableText style={{ marginTop: '5%' }}>
-                      <ScalableText style={{ fontSize: wid * 40, fontFamily: 'WSR', color: 'white' }}>{hours}</ScalableText>
-                      <ScalableText style={{ fontSize: wid * 20, fontFamily: 'WSR', color: 'white' }}>hours</ScalableText>
-                      <ScalableText style={{ fontSize: wid * 40, fontFamily: 'WSR', color: 'white' }}> {minutes}</ScalableText>
-                      <ScalableText style={{ fontSize: wid * 20, fontFamily: 'WSR', color: 'white' }}>minutes</ScalableText>
-                    </ScalableText>
-                  </View>
-                  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', flexDirection: 'row' }}>
-                    <Progress.Bar progress={this.state.progress2} width={entireScreenWidth * 0.5} animated={true} height={rem * 15} borderRadius={25} color='#BBE2FF' borderColor='#D0D0D0' unfilledColor='white' />
-                  </View>
-                </View>
-                <View style={{ height: '90%', flex: 1 }}>
-                  <Image source={require('../assets/night.png')} style={{
-                    height: '100%',
-                    width: '90%',
-                    flex: 1,
-                  }} resizeMode="contain"></Image>
-                </View>
+              <View style={{ height: '90%', flex: 1 }}>
+                <Image source={require('../assets/night.png')} style={{
+                  height: '100%',
+                  width: '90%',
+                  flex: 1,
+                }} resizeMode="contain"></Image>
               </View>
             </View>
-            <View style={{ flex: 12, width: '100%', alignItems: 'center' }}>
-              <View style={styles.midcard2}>
-                <View style={{ height: '100%', flex: 1 }}>
-                  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', flexDirection: 'row' }}>
-                    <ScalableText style={{ marginTop: '5%' }}>
-                      <ScalableText style={{ fontSize: wid * 22, fontFamily: 'WSR', color: 'white' }}>License Date:</ScalableText>
-                    </ScalableText>
-                  </View>
-                  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', flexDirection: 'row', justifyContent: 'center', width: '100%' }}>
-                    <View style={{ width: '80%', height: '80%', borderRadius: 10, backgroundColor: '#D0D0D0', borderColor: 'white', borderWidth: 2, marginBottom: '20%', alignItems: 'center', justifyContent: 'center' }}>
-                      <DatePicker
-                        style={{ width: 120 * wid, marginBottom: 10 * ree, }}
-                        date={this.state.date}
-                        mode="date"
-                        minDate={moment().add(1, 'days').format("MM-DD-YYYY")}
-                        format="MM-DD-YYYY"
-                        confirmBtnText="Confirm"
-                        cancelBtnText="Cancel"
-                        showIcon={false}
-                        placeholder=' '
+          </View>
+          <View style={{ flex: 12, width: '100%', alignItems: 'center' }}>
+            <View style={styles.midcard2}>
+              <View style={{ height: '100%', flex: 1 }}>
+                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', flexDirection: 'row' }}>
+                  <Text style={{ marginTop: '5%' }}>
+                    <Text style={{ fontSize: wid * 22, fontFamily: 'WSR', color: 'white' }}>License Date:</Text>
+                  </Text>
+                </View>
+                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', flexDirection: 'row', justifyContent: 'center', width: '100%' }}>
+                  <View style={{ width: '80%', height: '80%', borderRadius: 10, backgroundColor: '#D0D0D0', borderColor: 'white', borderWidth: 2, marginBottom: '20%', alignItems: 'center', justifyContent: 'center' }}>
+                    <DatePicker
+                      style={{ width: 120 * wid, marginBottom: 10 * ree, }}
+                      date={this.state.date}
+                      mode="date"
+                      minDate={moment().add(1, 'days').format("MM-DD-YYYY")}
+                      format="MM-DD-YYYY"
+                      confirmBtnText="Confirm"
+                      cancelBtnText="Cancel"
+                      showIcon={false}
+                      placeholder=' '
 
-                        customStyles={{
+                      customStyles={{
 
-                          dateInput: { borderWidth: 0 },
-                          dateText: {
-                            fontSize: 20 * wid,
-                            color: 'white',
-                            fontFamily: 'WSR'
+                        dateInput: { borderWidth: 0 },
+                        dateText: {
+                          fontSize: 20 * wid,
+                          color: 'white',
+                          fontFamily: 'WSR'
+                        }
+                        // ... You can check the source to find the other keys.
+                      }}
+                      onDateChange={(date) => {
+                        this.setState({ date: date });
+                        var a = moment();
+                        var b = moment(date, 'MM-DD-YYYY')
+                        const mins = hours * 60 + minutes;
+                        if (mins > 3000) {
+                          this.setState({ hoursneeded: 'Done!' })
+                        }
+                        else {
+                          const hrs = b.diff(a, 'days') + 1
+                          if (hrs < 7) {
+                            var needed = (3000 - mins) / 60;
+                            needed = Math.round((needed + Number.EPSILON) * 100) / 100
+                            this.setState({ hoursneeded: String(needed) })
                           }
-                          // ... You can check the source to find the other keys.
-                        }}
-                        onDateChange={(date) => {
-                          this.setState({ date: date });
-                          var a = moment();
-                          var b = moment(date, 'MM-DD-YYYY')
-                          const mins = hours * 60 + minutes;
-                          if (mins > 3000) {
-                            this.setState({ hoursneeded: 'Done!' })
-                          }
+
                           else {
-                            const hrs = b.diff(a, 'days') + 1
-                            if (hrs < 7) {
-                              var needed = (3000 - mins) / 60;
-                              needed = Math.round((needed + Number.EPSILON) * 100) / 100
-                              this.setState({ hoursneeded: String(needed) })
-                            }
-
-                            else {
-                              console.log(hrs)
-                              const weeks = Math.round(hrs / 7)
-                              var needed = (2400 - mins) / weeks / 60;
-                              needed = Math.round((needed + Number.EPSILON) * 100) / 100
-                              this.setState({ hoursneeded: String(needed) })
-                            }
+                            console.log(hrs)
+                            const weeks = Math.round(hrs / 7)
+                            var needed = (2400 - mins) / weeks / 60;
+                            needed = Math.round((needed + Number.EPSILON) * 100) / 100
+                            this.setState({ hoursneeded: String(needed) })
                           }
                         }
-                        }
+                      }
+                      }
 
-                      />
-                    </View>
-                  </View>
-                </View>
-                <View style={{ height: '90%', flex: 0.05, alignItems: 'center', justifyContent: 'center' }}>
-                  <Progress.Bar progress={0} width={entireScreenWidth * 0.9 * 0.1 / 2.1 * 0.5} animated={true} height={rem * 46} borderRadius={25} color='#BBE2FF' borderColor='#D0D0D0' unfilledColor='white' />
-                </View>
-                <View style={{ height: '90%', flex: 1 }}>
-                  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', flexDirection: 'row' }}>
-                    <ScalableText style={{ marginTop: '14%' }}>
-                      <ScalableText style={{ fontSize: wid * 18, fontFamily: 'WSR', color: 'white' }}>Hours Per Week:</ScalableText>
-                    </ScalableText>
-                  </View>
-                  <View style={{ flex: 2, justifyContent: 'center', alignItems: 'center', flexDirection: 'row' }}>
-                    <ScalableText style={{ marginTop: '0%' }}>
-                      <ScalableText style={{ fontSize: wid * 50, fontFamily: 'WSR', color: 'white' }}>{this.state.hoursneeded}</ScalableText>
-                    </ScalableText>
+                    />
                   </View>
                 </View>
               </View>
-            </View>
-            <View style={{ flex: 15, alignItems: 'center', justifyContent: 'center', width: '100%' }}>
-              <View style={styles.topcard}>
-                <View style={{ flex: 1, alignItems: 'center', maxWidth: '90%' }}>
-                  <ScalableText style={{ fontSize: wid * 25, fontFamily: 'WSB', color: 'white', flex: 1, marginTop: '5%' }}>Teacher Comments: </ScalableText>
-                  <View style={{ flex: 4 }}>
-                    <ScrollView style={{ width: '100%', flex: 4 }} bounces={false}>
-                      <ScalableText style={{ fontSize: wid * 25, fontFamily: 'WSR', color: 'white' }}>{comments}</ScalableText>
-                    </ScrollView>
-                  </View>
+              <View style={{ height: '90%', flex: 0.05, alignItems: 'center', justifyContent: 'center' }}>
+                <Progress.Bar progress={0} width={entireScreenWidth * 0.9 * 0.1 / 2.1 * 0.5} animated={true} height={rem * 46} borderRadius={25} color='#BBE2FF' borderColor='#D0D0D0' unfilledColor='white' />
+              </View>
+              <View style={{ height: '90%', flex: 1 }}>
+                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', flexDirection: 'row' }}>
+                  <Text style={{ marginTop: '14%' }}>
+                    <Text style={{ fontSize: wid * 17, fontFamily: 'WSR', color: 'white' }}>Hours Per Week:</Text>
+                  </Text>
                 </View>
-
+                <View style={{ flex: 2, justifyContent: 'center', alignItems: 'center', flexDirection: 'row' }}>
+                  <Text style={{ marginTop: '0%' }}>
+                    <Text style={{ fontSize: wid * 50, fontFamily: 'WSR', color: 'white' }}>{this.state.hoursneeded}</Text>
+                  </Text>
+                </View>
               </View>
             </View>
+          </View>
+          <View style={{ flex: 15, alignItems: 'center', justifyContent: 'center', width: '100%' }}>
+            <View style={styles.topcard}>
+              <View style={{ flex: 1, alignItems: 'center', maxWidth: '90%' }}>
+                <Text style={{ fontSize: wid * 25, fontFamily: 'WSB', color: 'white', flex: 1, marginTop: '5%' }}>Teacher Comments: </Text>
+                <View style={{ flex: 4 }}>
+                  <ScrollView style={{ width: '100%', flex: 4 }} bounces={false}>
+                    <Text style={{ fontSize: wid * 25, fontFamily: 'WSR', color: 'white' }}>{comments}</Text>
+                  </ScrollView>
+                </View>
+              </View>
+
+            </View>
+          </View>
+          <View style={{
+            width: '73%',
+            flex: 8,
+            paddingBottom: '2%',
+            paddingTop: '2%',
+            justifyContent: 'center',
+            alignItems: 'center'
+
+          }}>
+            <TouchableOpacity
+              style={{
+                flex: 1,
+                width: entireScreenHeight / 8 * 0.96,
+              }}
+              onPress={onPress}
+              disabled={this.state.loading}
+
+            >
+              <Image source={require('../assets/backbut.png')} style={{
+                height: '100%',
+                width: '100%',
+                flex: 1
+
+
+              }} resizeMode="contain"></Image>
+            </TouchableOpacity>
           </View>
         </ImageBackground>
       </View>
@@ -200,13 +234,6 @@ const styles = StyleSheet.create({
     height: '100%',
     resizeMode: 'cover',
     alignItems: 'center',
-  },
-  imagefront: {
-    marginTop: '8%',
-    height: '25%',
-    width: '80%',
-    flex: 2,
-
   },
   topcard: {
     height: '90%', width: '90%', backgroundColor: '#D0D0D0', borderRadius: 25, shadowColor: "#000",
